cbabff1321c3209c00ed63d745d2b522f974d99e
Queisme <queiskey@gmail.com>
2024-07-25 20:17:02 -0500

    update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = (arrs, ...reMove) => {
+  const trying = new Set(reMove);
+  return arrs.filter((value) => !trying.has(value));
 };
 
 // Do not edit below this line
