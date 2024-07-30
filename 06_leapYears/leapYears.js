a0c9b2a84ad329bdbd0b3b95beb3666b6565adc5
Queisme <queiskey@gmail.com>
2024-07-30 18:00:37 -0500

    JS exercises 1 thru 7 answered

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,9 @@
-const leapYears = (year) => {
-  if (year % 4 === 0) return true;
-  if (year % 100 === 0 && year % 400 === 0) return true;
-  else return false;
-};
+// const leapYears = (year) => {
+//   return new Date(year, 1, 29).getMonth() === 1;
+// };
+
+const leapYears = (year) =>
+  (0 === year % 4 && 0 !== year % 100) || 0 === year % 400 ? true : false;
 
 // Do not edit below this line
 module.exports = leapYears;
