4fe0db6e4780fa87c872feb6087910b501de99f9
Queisme <queiskey@gmail.com>
2024-08-06 18:13:07 -0500

    10 - fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function fib(n) {
+  if (n < 0) return 'OOPS';
+  if (n === '0') return 0;
+  let a = 0,
+    b = 1,
+    c,
+    i;
+  if (n === 0) return a;
+  for (i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 };
 
 // Do not edit below this line
